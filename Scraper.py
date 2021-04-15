21479d86f5746ba665623243b7436ba34837bbe8
Chainy Carson <cdcarson@ucsc.edu>
2021-04-14 17:42:20 -0700

    Second commit, got description reading working

--- Scraper.py
+++ Scraper.py
@@ -1,21 +1,54 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import options
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
 
 
-#Code here used from https://www.scrapingbee.com/blog/selenium-python/
+class Scraper:
+	def __init__ (self, is_headless = True):
+		#puts the browser in headless mode (no UI) if is_headless is true
+		if is_headless: 
+			self.current_options = Options()
+			self.current_options.headless = True
+			self.driver = webdriver.Chrome(options=self.current_options)
+		#set the driver path
+		else:
+			self.driver = webdriver.Chrome()
 
-#set the options to run in headless, so no GUI
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
+	#navigates the scraper to a page at the given URL
+	def get_page(self, url):
+		self.driver.get(url)
+		self.driver.implicitly_wait(5)
+		sleep(10)
+	#returns the title at the current page
+	def get_title(self):
+		return self.driver.title
+	#simply eliminates this browser instance
+	def end_scraping(self):
+		self.driver.quit()
+	#this scrapes the job details from a given Linkedin page
+	def scrape_linkedin(self):
+		self.driver.find_element_by_class_name("show-more-less-html__button").click()
+		print(self.driver.find_element_by_class_name("description").text, file = open("output2.txt","w"))
+		finder = BeautifulSoup(self.driver.page_source, 'html5lib')
+		job_description = finder.find('div', 
+									attrs= {'class': 'jobs-box__html-content jobs-description-content__text t-14 t-normal'})
+		print(finder.prettify(), file = open("output.txt","w"))
+		print(job_description)
+		#text = job_description.get_text()
 
-#set the driver path
-DRIVER_PATH = './chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
-#TODO: integrate this so the url here is dynamic. for current purposes,
-#This will just do one page. We will make it dynamic in the future.
-driver.get('https://google.com')
 
-print(driver.page_source)
-driver.quit()
+		return 
+
+
+def main():
+	test = Scraper(False)
+	test.get_page('https://www.linkedin.com/jobs/search/?currentJobId=2500876429&start=0')
+	test.scrape_linkedin()
+	#test.end_scraping()
+
+if __name__ == "__main__":
+	main()
