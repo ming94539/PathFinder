7e7d647c17d1eed45ad63662f5bcb5204205ae3b
Chainy Carson <cdcarson@ucsc.edu>
2021-04-17 21:48:06 -0700

    Committing prototype for integrating with crawler

--- Scraper.py
+++ Scraper.py
@@ -21,7 +21,7 @@ class Scraper:
 	def get_page(self, url):
 		self.driver.get(url)
 		self.driver.implicitly_wait(5)
-		sleep(10)
+		sleep(2)
 	#returns the title at the current page
 	def get_title(self):
 		return self.driver.title
@@ -31,24 +31,15 @@ class Scraper:
 	#this scrapes the job details from a given Linkedin page
 	def scrape_linkedin(self):
 		self.driver.find_element_by_class_name("show-more-less-html__button").click()
-		print(self.driver.find_element_by_class_name("description").text, file = open("output2.txt","w"))
-		finder = BeautifulSoup(self.driver.page_source, 'html5lib')
-		job_description = finder.find('div', 
-									attrs= {'class': 'jobs-box__html-content jobs-description-content__text t-14 t-normal'})
-		print(finder.prettify(), file = open("output.txt","w"))
-		print(job_description)
-		#text = job_description.get_text()
-
-
-
+		print(self.driver.find_element_by_class_name("description").text, file = open("results.txt","a"))
 		return 
 
 
 def main():
-	test = Scraper(False)
-	test.get_page('https://www.linkedin.com/jobs/search/?currentJobId=2500876429&start=0')
+	test = Scraper()
+	test.get_page('https://www.linkedin.com/jobs/search/?currentJobId=2488392591&geoId=103644278&keywords=software%20engineer&location=United%20States&position=3&pageNum=0')
 	test.scrape_linkedin()
-	#test.end_scraping()
+	test.end_scraping()
 
 if __name__ == "__main__":
 	main()
