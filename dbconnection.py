bda92bf91ad16c67509252d0448c58f8d83c4fb6
EricNgor <EricNgor@users.noreply.github.com>
2021-05-09 18:53:51 -0700

    DBConnection supports series of statements

--- dbconnection.py
+++ dbconnection.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import Session, sessionmaker
-from os.path import dirname, join, realpath
 import sys
 
 class DBConnection(object):
     def __init__(self):
+        """Initialize DBConnection object to interact with database"""
+        
         user='acraig1225'
         pw='#$%cse115a#$%'
         host='highlanderdb.cjnqiycyzhsl.us-east-2.rds.amazonaws.com'
@@ -12,16 +13,30 @@ class DBConnection(object):
         conn_string = f"postgresql://{user}:{pw}@{host}/{db_name}"
 
         self.engine = create_engine(conn_string)
+        self.stmts = []
 	
-    def execute(self, query):
+    def execute(self):
+        """
+        Sequentially execute each SQL statement, then 
+        clear list of statements
+        """
         Session = sessionmaker(self.engine)
         session = Session()
 
-        try:
-            session.execute(text(query))
-            session.commit()
-        except Exception as e:
-            print(e, file=sys.stderr)
-            session.rollback()
+        for stmt in self.stmts:
+            try:
+                session.execute(text(stmt))
+                session.commit()
+            except Exception as e:
+                print(e, file=sys.stderr)
+                session.rollback()
 
+        self.stmts = []
         session.close()
+
+    def add_stmt(self, stmt):
+        self.stmts.append(stmt)
+
+    def query(self, query):
+        """Executes a query and returns list of results"""
+        return list(self.engine.execute(text(query)))
