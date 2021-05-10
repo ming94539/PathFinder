dff648168a135b65ecdba3572644e82ed412ad8e
Andrew <acraig1225@gmail.com>
2021-05-10 16:07:46 -0700

    updated tables based on data standards

--- tables.sql
+++ tables.sql
@@ -6,32 +6,32 @@ CREATE TABLE JobIDTable (
 
 CREATE TABLE Industries (
   jobID INTEGER PRIMARY KEY NOT NULL,
-  industry VARCHAR(60),
+  industry VARCHAR(50) NOT NULL,
   jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
 );
 
 CREATE TABLE Education (
   jobID INTEGER PRIMARY KEY NOT NULL,
-  degreeTitle VARCHAR(15),
-  educationLevel VARCHAR(15),
+  degreeTitle VARCHAR(50),
+  educationLevel VARCHAR(1),
   jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
 );
 
 CREATE TABLE Skills (
   jobID INTEGER PRIMARY KEY NOT NULL,
-  skill VARCHAR(60),
+  skill VARCHAR(60) NOT NULL,
   jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
 );
 
 CREATE TABLE Languages (
   jobID INTEGER PRIMARY KEY NOT NULL,
-  language VARCHAR(15),
+  language VARCHAR(30) NOT NULL,
   jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
 );
 
 CREATE TABLE WebDeveloper (
 	jobID INTEGER NOT NULL,
-	seniority VARCHAR(60),
+	seniority VARCHAR(30),
 	yearsOfExperience INTEGER,
 	PRIMARY KEY (jobID),
 	FOREIGN KEY (jobID) REFERENCES JobIDTable(jobID)
