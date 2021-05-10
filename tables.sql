8260db0d6e38fe9341dc08a25a0e2dbcb35239c6
Andrew <acraig1225@gmail.com>
2021-05-09 19:45:38 -0700

    added attribute tables

--- tables.sql
+++ tables.sql
@@ -1,19 +1,40 @@
 CREATE TYPE education_value AS ENUM ('bachleor', 'master', 'doctorate');
 
 CREATE TABLE JobIDTable (
-	jobID INTEGER,
+	jobID INTEGER NOT NULL,
 	table VARCHAR(60),
 	PRIMARY KEY (jobID)
+);
+
+CREATE TABLE Industries (
+  jobID INTEGER PRIMARY KEY NOT NULL,
+  industry VARCHAR(60),
+  jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
+);
+
+CREATE TABLE Education (
+  jobID INTEGER PRIMARY KEY NOT NULL,
+  degreeTitle VARCHAR(15),
+  educationLevel VARCHAR(15),
+  jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
+);
+
+CREATE TABLE Skills (
+  jobID INTEGER PRIMARY KEY NOT NULL,
+  skill VARCHAR(60),
+  jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
+);
+
+CREATE TABLE Languages (
+  jobID INTEGER PRIMARY KEY NOT NULL,
+  language VARCHAR(15),
+  jobID INTEGER FOREIGN KEY REFERENCES JobIDTable(jobID)
 );
 
 CREATE TABLE WebDeveloper (
 	jobID INTEGER NOT NULL,
 	seniority VARCHAR(60),
-	industry VARCHAR(60),
-	educationLevel education_value,
-	degreeTitle VARCHAR(60),
-	skills json NOT NULL,
-	language json NOT NULL,
+	yearsOfExperience INTEGER,
 	PRIMARY KEY (jobID),
-	FOREIGN KEY (jobID) REFERENCES JobIDTable
+	FOREIGN KEY (jobID) REFERENCES JobIDTable(jobID)
 ); 
