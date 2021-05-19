c6142968109078608a42268e03ed3947849a709e
EricNgor <EricNgor@users.noreply.github.com>
2021-05-19 15:41:11 -0700

    push

--- backend/src/table_data.js
+++ backend/src/table_data.js
@@ -1,5 +1,15 @@
 const db = require('./db');
 
+function formatData(data) {
+  let result = {};
+  for (let index in data) {
+    result[index] = data
+  }
+  console.log('result:', result)
+
+  return result;
+}
+
 // demand MUST match the alias used in its query template (ex: 's' for Skills s)
 // job should match one of our 
 exports.getData = async (req, res) => {
@@ -25,7 +35,7 @@ exports.getData = async (req, res) => {
 
   // Most popular skill 
   let skillDemand = `
-    SELECT s.skill, COUNT(*) AS count
+    SELECT s.skill as value, COUNT(*) AS count
     FROM Skills s ${jobTitleMatch}
     GROUP BY s.skill
     ORDER BY count DESC
@@ -43,9 +53,10 @@ exports.getData = async (req, res) => {
 
   console.log('USING QUERY:', query);
   const grab_all = await db.dbGet(query);
+
   if (grab_all) {
     //formatting
-    console.log('grabbed:', grab_all)
+    formatData(grab_all)
     res.status(200).json(grab_all);
     return;
   } else {
