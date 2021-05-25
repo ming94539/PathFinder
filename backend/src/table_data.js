b0f8dde2983f96d4cba490fb13a11989c80e629a
liangalice <aliceliang1110@gmail.com>
2021-05-25 02:42:00 -0700

    modifed for most popular fields query

--- backend/src/table_data.js
+++ backend/src/table_data.js
@@ -21,6 +21,8 @@ exports.getData = async (req, res) => {
   switch(demand) {
     case('Most in Demand Skills'):
       demand='s'; break;
+    case('Most Popular Fields'):
+      demand='f'; break;
     default:
       break;
   }
@@ -38,6 +40,7 @@ exports.getData = async (req, res) => {
     GROUP BY s.skill
     ORDER BY count DESC
   `;
+  
   // referenced and altered from: https://www.sisense.com/blog/exact-row-counts-for-every-database-table/
   let rowsOfEachTable = `
     select table_schema, table_name, (xpath('/row/cnt/text()', xml_count))[1]::text::int as row_count
@@ -48,12 +51,23 @@ exports.getData = async (req, res) => {
     where table_schema = 'public' 
     ) t order by row_count DESC`;
 
+  let rowsOfJobTitles = `select table_schema, table_name as value, (xpath('/row/cnt/text()', xml_count))[1]::text::int as count
+    from (                             
+      select table_name, table_schema, 
+        query_to_xml(format('select count(*) as cnt from %I.%I', table_schema, table_name), false, true, '') as xml_count
+        from information_schema.tables
+    where table_schema = 'public' and table_name != 'skills' and table_name != 'languages' and table_name != 'industries' 
+    and table_name != 'jobidtable' and table_name != 'education' 
+    ) t order by count DESC`;
+
   let query = '';
   switch(demand) {
     case 's':
       query = skillDemand;
       break;
-    // other cases here
+    case 'f':
+      query = rowsOfJobTitles;
+      break;
     default:
       break;
   }
@@ -62,7 +76,6 @@ exports.getData = async (req, res) => {
   const grab_all = await db.dbGet(query);
 
   if (grab_all) {
-    //formatting
     console.log("200 status", grab_all);
     res.status(200).json(grab_all);
     return;
